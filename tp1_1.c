feae4027e4964db5ea30e31660a77f32497e9450
gudiino <gudino1387@gmail.com>
2022-03-20 16:53:40 -0300

    ingreso archivo .gitignore y desarrollo ejercicio punteros

--- tp1_1.c
+++ tp1_1.c
@@ -2,5 +2,13 @@
 
 int main(){
     printf("Hola mundo!\n");
+    int num, *p;
+    num = 10;
+    p=&num;
+    printf("Contenido del puntero: *p = %d\n", *p);
+    printf("Direccion de memoria almacenada por el puntero: *p = %d\n", p);
+    printf("Direccion de memoria de la variable: num = %d\n", &num);
+    printf("Direccion de memoria del puntero: *p = %d\n", &p);
+    printf ("Tamaño de memoria usado por la variable num es de %zd bytes\n", sizeof (int));
     return 0;
 }
